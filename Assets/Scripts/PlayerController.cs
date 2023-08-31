eee8409a1ec0693d16bd46c72789c88d91221913
NewGameStudio <andre-orsk@yandex.ru>
2023-08-31 12:12:54 +0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _horizontalSpeed;
+
+    [SerializeField]
+    private float _fallSpeed;
+
+    [SerializeField]
+    private float _liftSpeed;
+
+    private void Update()
     {
-        
+        Vector3 position = transform.position;
+
+        MoveHorizontal(ref position);
+        MoveVertical(ref position);
+
+        transform.position = position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveHorizontal(ref Vector3 position)
     {
-        
+        position += transform.right * _horizontalSpeed * Time.deltaTime;
+    }
+
+    private void MoveVertical(ref Vector3 position)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            position += transform.up * _liftSpeed * Time.deltaTime;
+        }
+        else
+        {
+            position -= transform.up * _fallSpeed * Time.deltaTime;
+        }
     }
 }
