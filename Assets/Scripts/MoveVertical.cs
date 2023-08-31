4fae7b622ec9d8307d5451a51398920d38062a97
NewGameStudio <andre-orsk@yandex.ru>
2023-08-31 12:34:14 +0500

    Update MoveVertical.cs

--- Assets/Scripts/MoveVertical.cs
+++ Assets/Scripts/MoveVertical.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class MoveVertical : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Distance
     {
-        
+        get
+        {
+            return _distance;
+        }
+
+        set
+        {
+            _distance = Mathf.Max(0.1f, value);
+        }
+    }
+    public float Speed
+    {
+        get
+        {
+            return _speed;
+        }
+
+        set
+        {
+            _speed = Mathf.Max(0, value);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private float _distance;
+
+    [SerializeField]
+    private float _speed;
+
+    private void Update()
     {
-        
+        Vector3 localPosition = transform.localPosition;
+
+        localPosition.y = Mathf.Sin(Time.time * _speed) * _distance;
+
+        transform.localPosition = localPosition;
     }
 }
